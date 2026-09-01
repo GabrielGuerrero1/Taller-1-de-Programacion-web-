dbe3ddfdf30ff26f920b706e3f03e5caee2a3a36
RamirezJavi <00016824@uca.edu.sv>
2026-09-01 14:51:43 -0600

    Agrega ejercicio 2 clasificacion de notas

--- Taller1.js
+++ Taller1.js
@@ -11,3 +11,26 @@ console.log("Producto: " + nombreProducto);
 console.log("Precio original: $" + precioProducto);
 console.log("Descuento: " + porcentajeDescuento + "%");
 console.log("Precio final: $" + precioFinal);
+
+// EJERCICIO 2 - CLASIFICACION DE NOTAS
+
+let nota = 85;
+
+if (nota >= 90 && nota <= 100) {
+    console.log("Excelente");
+
+} else if (nota >= 80 && nota <= 89) {
+    console.log("Muy Bueno");
+
+} else if (nota >= 70 && nota <= 79) {
+    console.log("Bueno");
+
+} else if (nota >= 60 && nota <= 69) {
+    console.log("Regular");
+
+} else if (nota >= 0 && nota < 60) {
+    console.log("Reprobado");
+
+} else {
+    console.log("Nota invalida");
+}
